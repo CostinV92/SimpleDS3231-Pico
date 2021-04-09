e09b76b49da620ae613d22a780a72a2f495ed2a6
Victor Gabriel Costin <costinv92@gmail.com>
2021-04-09 11:18:32 +0300

    DS3231 Raspbery Pi Pico driver
    Signed-off-by: Victor Gabriel Costin <costinv92@gmail.com>

---
+++ examples/SimpleDS3231Example.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "SimpleDS3231.h"
+
+int main() {
+    int state = 0;
+    SimpleDS3231 rtc;
+
+    stdio_init_all();
+
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, state);
+
+    while (true) {
+        gpio_put(PICO_DEFAULT_LED_PIN, !state);
+        state = !state;
+
+        printf("\r%s -- %s -- %d%cC", rtc.get_date_str(), rtc.get_time_str(), rtc.get_temp(), 248);
+
+        sleep_ms(500);
+    }
+
+    return 0;
+}
